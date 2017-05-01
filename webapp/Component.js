fe85a864b6160233600e4ee680efc10e67e16b49
githubwiserhai <wise.rhai@gmail.com>
2017-05-01 17:02:56 +0000

    Base copy; Commit and Push (to master)

---
+++ webapp/Component.js
@@ -0,0 +1,32 @@
+/**Historical changes 
+ * 1) Base copy of master
+
+*/
+
+sap.ui.define([
+	"sap/ui/core/UIComponent",
+	"sap/ui/Device",
+	"Git_prjGitExploration01/model/models"
+], function(UIComponent, Device, models) {
+	"use strict";
+
+	return UIComponent.extend("Git_prjGitExploration01.Component", {
+
+		metadata: {
+			manifest: "json"
+		},
+
+		/**
+		 * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
+		 * @public
+		 * @override
+		 */
+		init: function() {
+			// call the base component's init function
+			UIComponent.prototype.init.apply(this, arguments);
+
+			// set the device model
+			this.setModel(models.createDeviceModel(), "device");
+		}
+	});
+});
